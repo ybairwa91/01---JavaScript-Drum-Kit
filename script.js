a44d196eedfc52e85c506f13c6d7f516f950ee3f
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-15 17:38:16 +0530

    ..

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+/*Ensure Proper Loading: Ensure that your JavaScript code is executed after the HTML document has finished loading. You can do this by placing your JavaScript code inside an event listener that listens for the "DOMContentLoaded" event.
+ */
 document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("keydown", function (e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // console.log(e); //tell the which key is pressed or simply say keydown
 
-    const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //that how we select an attribute property of an element we syntax= tagName[property to select].
 
+    //selected key element from the html code and its attribute as well
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    //if keypress gives which doesnot matches with the given values ,use this way to stop the function using return.
     if (!audio) return; //stop the function all together
-    audio.currentTime = 0; //rewind to the start.
-    audio.play();
+    audio.currentTime = 0; //rewind to the start always whenever u keypress.
+    audio.play(); //in js play method helps to play the audio which is stored in html code using audio tag and store into its attribute.
 
     var element = document.querySelector(".key");
-
-    // key.className.add("playing");
+    key.classList.add("playing");
   });
+
+  const keys = document.querySelectorAll(".key"); //gives nodelist,what is nodelist-->A NodeList is an ordered list of DOM nodes. These nodes can be elements, text nodes, attributes, etc., found in the document.
+  //   something like which resembles array but not exactly array it has same properties as array do like using index to get the element of nodelist,but nodelist are live in nature it means whenever we made any changes it automatically apply and give live output .
+
+  function removeTransition(e) {
+    if (e.propertyName != "transform") return;
+    console.log(e.propertyName);
+    console.log(this);
+    this.classList.remove("playing");
+  }
+  keys.forEach((key) =>
+    key.addEventListener("transitionend", removeTransition)
+  );
 });
