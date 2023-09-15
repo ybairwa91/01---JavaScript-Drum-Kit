aa572632e8845a3e18b53cea3c633b9d6a1a0d70
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-15 17:45:13 +0530

    ....

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 /*Ensure Proper Loading: Ensure that your JavaScript code is executed after the HTML document has finished loading. You can do this by placing your JavaScript code inside an event listener that listens for the "DOMContentLoaded" event.
  */
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("keydown", function (e) {
-    // console.log(e); //tell the which key is pressed or simply say keydown
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //that how we select an attribute property of an element we syntax= tagName[property to select].
+function playSound(e) {
+  // console.log(e); //tell the which key is pressed or simply say keydown
 
-    //selected key element from the html code and its attribute as well
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //that how we select an attribute property of an element we syntax= tagName[property to select].
 
-    //if keypress gives which doesnot matches with the given values ,use this way to stop the function using return.
-    if (!audio) return; //stop the function all together
-    audio.currentTime = 0; //rewind to the start always whenever u keypress.
-    audio.play(); //in js play method helps to play the audio which is stored in html code using audio tag and store into its attribute.
+  //selected key element from the html code and its attribute as well
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-    var element = document.querySelector(".key");
-    key.classList.add("playing");
-  });
+  //if keypress gives which doesnot matches with the given values ,use this way to stop the function using return.
+  if (!audio) return; //stop the function all together
+  audio.currentTime = 0; //rewind to the start always whenever u keypress.
+  audio.play(); //in js play method helps to play the audio which is stored in html code using audio tag and store into its attribute.
 
-  const keys = document.querySelectorAll(".key"); //gives nodelist,what is nodelist-->A NodeList is an ordered list of DOM nodes. These nodes can be elements, text nodes, attributes, etc., found in the document.
-  //   something like which resembles array but not exactly array it has same properties as array do like using index to get the element of nodelist,but nodelist are live in nature it means whenever we made any changes it automatically apply and give live output .
+  var element = document.querySelector(".key");
+  key.classList.add("playing");
+}
 
-  function removeTransition(e) {
-    if (e.propertyName != "transform") return;
-    console.log(e.propertyName);
-    console.log(this);
-    this.classList.remove("playing");
-  }
-  keys.forEach((key) =>
-    key.addEventListener("transitionend", removeTransition)
-  );
-});
+window.addEventListener("keydown", playSound);
+const keys = document.querySelectorAll(".key"); //gives nodelist,what is nodelist-->A NodeList is an ordered list of DOM nodes. These nodes can be elements, text nodes, attributes, etc., found in the document.
+//   something like which resembles array but not exactly array it has same properties as array do like using index to get the element of nodelist,but nodelist are live in nature it means whenever we made any changes it automatically apply and give live output .
+
+function removeTransition(e) {
+  if (e.propertyName != "transform") return;
+  console.log(e.propertyName);
+  console.log(this);
+  this.classList.remove("playing");
+}
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
