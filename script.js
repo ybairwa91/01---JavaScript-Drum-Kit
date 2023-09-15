5f6dcf9b5f8a7f74c6b496265c0cf89bf6480d66
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-15 16:37:46 +0530

    NEW PROJECT

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("keydown", function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    const key = document.querySelector(`.key[data-key="${e.keycode}"]`);
+
+    if (!audio) return; //stop the function all together
+    audio.currentTime = 0; //rewind to the start.
+    audio.play();
+
+    var element = document.querySelector(".key");
+
+    // key.className.add("playing");
+  });
+});
